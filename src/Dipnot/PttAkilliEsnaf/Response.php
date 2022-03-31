643df60ee1fb9de15095c6c4ad64cf2eb654ea1d
Özgür Görgülü <ozgurgorgulu54@gmail.com>
2022-03-31 11:12:32 +0300

    Add Response.php

---
+++ src/Dipnot/PttAkilliEsnaf/Response.php
@@ -0,0 +1,24 @@
+<?php
+namespace Dipnot\PttAkilliEsnaf;
+
+/**
+ * Class Response
+ * @package Dipnot\PttAkilliEsnaf
+ */
+class Response
+{
+    /**
+     * @var Config $_config
+     */
+    protected $_config;
+
+    /**
+     * Response constructor
+     *
+     * @param Config $config
+     */
+    public function __construct(Config $config)
+    {
+        $this->_config = $config;
+    }
+}
